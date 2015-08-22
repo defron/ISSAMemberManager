c9b04abd7aac124e3b1bd7cb76fa3a84825b106f
Vu Cao <cdhvu@yahoo.com>
2015-08-22 10:51:17 -0700

    Updated database.py and all test files to prevent SQL Injection.

--- tests/test_update.py
+++ tests/test_update.py
@@ -1,10 +1,10 @@
 import unittest
-from Test import Database
+from ISSA.MemberMailer.Objects.database import Database
 
 class TestUpdate(unittest.TestCase):
 	def test_update_member(self):
 		test_member = Database()
-		test = test_member.update(1, "people", firstname= "Thomas", lastname= "Brown", email="tbrown@gmail", phone="5034567890")
+		test = test_member.update(1, "People", firstname = "Thomas2", lastname = "Brown2", email = "tbrown2@gmail",phone = "5034567890")
 		self.assertEqual(test, "Success!")
 
 if __name__ == '__main__':
